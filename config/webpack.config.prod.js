0fc1e530188e88b3e23985d9ddbb2cbbfb01ce66
Tomasz Danczak <tomaszdanczak@gmail.com>
2020-10-07 19:55:12 +0200

    feat: copy development to production

---
+++ config/webpack.config.prod.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  devtool: "source-map",
+  mode: "development",
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    filename: "js/[name].js",
+    path: path.resolve(__dirname, "../", "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sass|scss)$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "template",
+      template: "src/templates/template.html",
+    }),
+  ],
+};
