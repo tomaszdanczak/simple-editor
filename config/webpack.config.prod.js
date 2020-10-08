c299e9f8b1f69d8594b5c5f722c9e6db2e959dde
Tomasz Danczak <tomaszdanczak@gmail.com>
2020-10-08 19:30:59 +0200

    feat: configurate mini-css-extract-plugin

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
@@ -24,6 +25,9 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: "template",
       template: "src/templates/template.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/style.css",
     }),
   ],
 };
